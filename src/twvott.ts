76ffb456f62f1f0bbdcda15404561ab928050513
thewilley <89783791+TheWilley@users.noreply.github.com>
2024-11-06 21:20:43 +0100

    refactor: split parseLine function into multiple smaller functions

--- src/twvott.ts
+++ src/twvott.ts
@@ -91,106 +91,198 @@ class TWVOTT {
   }
 
   /**
-   * Parses a line.
+   * Parses a line from page contents.
    * @param line The line to parse
-   * @param y The y coordinate of the line
+   * @param y The y coordinate to insert the parsed line
    */
   private parseLine(line: string, y: number) {
     const tokens = line.split(' ').filter((token) => token !== '');
 
-    // Handling text
     if (tokens[0] === '>') {
-      tokens.shift();
-      let x = 0;
+      this.handleTextLine(tokens.slice(1), y);
+    } else if (tokens[0] === '$') {
+      this.handlePixelLine(tokens.slice(1), y);
+    }
+  }
 
-      let layers = {
-        textColor: 'white',
-        backgroundColor: '',
-        fontSize: this.fontSize,
-        fontWeight: '',
-      };
+  /**
+   * Handles text tokens (>).
+   * @param tokens The tokens to handle
+   * @param y The y coordinate to insert the parsed line
+   */
+  private handleTextLine(tokens: string[], y: number) {
+    let x = 0;
 
-      for (let i = 0; i < tokens.length; i++) {
-        let token = tokens[i];
-        token = token.replace(/\¶/g, ' ');
+    let layers = {
+      textColor: 'white',
+      backgroundColor: '',
+      fontSize: this.fontSize,
+      fontWeight: '',
+    };
 
-        if (token.startsWith('#')) {
-          // Handle color tags for text and background
-          const tagLength = token.match(/#+/)?.[0].length;
-          const colorKey = token.slice(tagLength);
-          const color =
-            this.colors[colorKey as keyof typeof this.colors] ||
-            this.colors.white;
+    tokens.forEach((token) => {
+      token = token.replace(/\¶/g, ' ');
 
-          if (tagLength === 1) layers.textColor = color;
-          else if (tagLength === 2) layers.backgroundColor = color;
-        } else if (token.startsWith(':')) {
-          const command = token.slice(1);
-
-          if (command === 'b') {
-            layers.fontWeight = 'bold';
-          } else if (command === 'i') {
-            layers.fontWeight = 'italic';
-          } else if (command === 'n') {
-            layers.fontWeight = '';
-          } else if (!isNaN(+command)) {
-            layers.fontSize = Number(command);
-          }
-        } else {
-          this.setFont(layers);
-
-          // Calculate both widths seperatly
-          const textWidth = this.context.measureText(token).width;
-          const spaceWidth = this.context.measureText(' ').width;
-
-          // Used to remove background color for the last space after changing to another
-          const finalWidth = textWidth + spaceWidth;
-
-          if (layers.backgroundColor) {
-            this.context.fillStyle = layers.backgroundColor;
-            this.context.fillRect(x, y - 1, finalWidth, this.fontSize);
-          }
-
-          this.context.fillStyle = layers.textColor;
-          this.context.fillText(token, x, y);
-
-          // Move x position for the next token
-          x += finalWidth;
-        }
+      if (this.isColorTag(token)) {
+        this.applyColorTag(token, layers);
+      } else if (this.isCommandTag(token)) {
+        this.applyCommandTag(token, layers);
+      } else {
+        x = this.drawTextToken(token, x, y, layers);
       }
+    });
+  }
+
+  /**
+   * Handles pixel tokens ($).
+   * @param tokens The tokens to handle
+   * @param y The y coordinate to insert the parsed line
+   */
+  private handlePixelLine(tokens: string[], y: number) {
+    let x = 0;
+    let fontSize = this.fontSize;
+    let currentColor = '#FFFFFF';
+
+    tokens.forEach((token) => {
+      if (this.isColorTag(token)) {
+        currentColor = this.getColorFromTag(token);
+        this.context.fillStyle = currentColor;
+      } else if (this.isCommandTag(token)) {
+        fontSize = this.getFontSizeFromTag(token, fontSize);
+      } else {
+        x = this.drawPixelToken(token, x, y, fontSize);
+      }
+    });
+  }
+
+  /**
+   * Check if the token is a color tag (#).
+   * @param token The token to check.
+   * @returns A boolean value indicating if the token is a color tag
+   */
+  private isColorTag(token: string): boolean {
+    return token.startsWith('#');
+  }
+
+  /**
+   * Check if the token is a command tag (:).
+   * @param token The token to check.
+   * @returns A boolean value indicating if the token is a command tag
+   */
+  private isCommandTag(token: string): boolean {
+    return token.startsWith(':');
+  }
+
+  /**
+   * Applies color to a token.
+   * @param token The token to apply color to
+   * @param layers An object defining text properties
+   */
+  private applyColorTag(token: string, layers: any) {
+    const tagLength = token.match(/#+/)?.[0].length;
+    const colorKey = token.slice(tagLength);
+    const color =
+      this.colors[colorKey as keyof typeof this.colors] || this.colors.white;
+
+    if (tagLength === 1) {
+      layers.textColor = color;
+    } else if (tagLength === 2) {
+      layers.backgroundColor = color;
+    }
+  }
+
+  /**
+   * Applies a command to a token.
+   * @param token The token to apply the command to
+   * @param layers An object defining text properties
+   */
+  private applyCommandTag(token: string, layers: any) {
+    const command = token.slice(1);
+
+    if (command === 'b') {
+      layers.fontWeight = 'bold';
+    } else if (command === 'i') {
+      layers.fontWeight = 'italic';
+    } else if (command === 'n') {
+      layers.fontWeight = '';
+    } else if (!isNaN(+command)) {
+      layers.fontSize = Number(command);
+    }
+  }
+
+  /**
+   * Draws text based on token.
+   * @param token The token containing the text to draw.
+   * @param x The x position to draw the text
+   * @param y The y position to draw the text
+   * @param layers  An object defining text properties
+   * @returns The new x position
+   */
+  private drawTextToken(
+    token: string,
+    x: number,
+    y: number,
+    layers: any
+  ): number {
+    this.setFont(layers);
+
+    const textWidth = this.context.measureText(token).width;
+    const spaceWidth = this.context.measureText(' ').width;
+    const finalWidth = textWidth + spaceWidth;
+
+    if (layers.backgroundColor) {
+      this.context.fillStyle = layers.backgroundColor;
+      this.context.fillRect(x, y - 1, finalWidth, layers.fontSize);
     }
 
-    // Handling pixels
-    else if (tokens[0] === '$') {
-      tokens.shift();
-      let x = 0;
-      let fontSize = this.fontSize;
+    this.context.fillStyle = layers.textColor;
+    this.context.fillText(token, x, y);
 
-      let currentColor = '#FFFFFF';
+    return x + finalWidth;
+  }
 
-      tokens.forEach((token) => {
-        if (token.startsWith('#')) {
-          // Change the pixel color
-          const colorKey = token.slice(1);
-          currentColor =
-            this.colors[colorKey as keyof typeof this.colors] || '#FFFFFF';
-          this.context.fillStyle = currentColor;
-        } else if (token.startsWith(':')) {
-          const command = token.slice(1);
-          if (!isNaN(+command)) {
-            fontSize = Number(command);
-          }
-        } else {
-          // Draw pixels based on characters
-          for (const character of token) {
-            if (character === '0') {
-              this.context.fillRect(x, y, fontSize, fontSize);
-            }
-            x += fontSize;
-          }
-        }
-      });
+  /**
+   * Gets color from a tag
+   * @param token The token to get color from
+   * @returns The color
+   */
+  private getColorFromTag(token: string): string {
+    const colorKey = token.slice(1);
+    return this.colors[colorKey as keyof typeof this.colors] || '#FFFFFF';
+  }
+
+  /**
+   * Gets font size from a tag
+   * @param token The token to get size from
+   * @param defaultSize The default size
+   * @returns The font size
+   */
+  private getFontSizeFromTag(token: string, defaultSize: number): number {
+    const command = token.slice(1);
+    return !isNaN(+command) ? Number(command) : defaultSize;
+  }
+
+  /**
+   * Draws pixels based on token,
+   * @param token The token containing the pixels to draw
+   * @param x The x position to draw the pixel
+   * @param y The y poisiton to draw the pixel
+   * @param fontSize The size of the pixel
+   * @returns The new x position
+   */
+  private drawPixelToken(
+    token: string,
+    x: number,
+    y: number,
+    fontSize: number
+  ): number {
+    for (const character of token) {
+      if (character === '0') {
+        this.context.fillRect(x, y, fontSize, fontSize);
+      }
+      x += fontSize;
     }
+    return x;
   }
 
   /**
